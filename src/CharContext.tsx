83b85f554f743478e5bc912932b0e40cda9e4153
Brendon Silva <brendon.dasilva03@gmail.com>
2024-05-14 01:04:28 -0300

    feat: useContext added

--- src/CharContext.tsx
+++ src/CharContext.tsx
@@ -3,13 +3,17 @@ import React, { createContext, useContext, useState } from 'react';
 interface ContextType {
   page: number;
   setPage: React.Dispatch<React.SetStateAction<number>>;
-  age: number;
-  setAge: React.Dispatch<React.SetStateAction<number>>;
+  name: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  status: string;
+  setStatus: React.Dispatch<React.SetStateAction<string>>;
+  specie: string;
+  setSpecie: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const CharContext = createContext<ContextType | undefined>(undefined);
 
- const useCharContext = () => {
+const useCharContext = () => {
   const context = useContext(CharContext);
   if (!context) {
     throw new Error('useCharContext must be used within a CharProvider');
@@ -21,12 +25,14 @@ interface CharProviderProps {
   children: React.ReactNode;
 }
 
- const CharProvider: React.FC<CharProviderProps> = ({ children }) => {
+const CharProvider: React.FC<CharProviderProps> = ({ children }) => {
   const [page, setPage] = useState(1);
-  const [age, setAge] = useState(20);
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [specie, setSpecie] = useState('');
 
   return (
-    <CharContext.Provider value={{ page, setPage, age, setAge}}>
+    <CharContext.Provider value={{ page, setPage, name, setName, status, setStatus, specie, setSpecie }}>
       {children}
     </CharContext.Provider>
   );
